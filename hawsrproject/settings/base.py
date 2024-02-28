3ae570ea56f0825c4e50390bdbbde42329794f94
Blazskills <ilesanmiisaac@gmail.com>
2024-02-28 19:35:15 +0100

    feat/ created app structure

---
+++ hawsrproject/settings/base.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+import environ
+from datetime import timedelta
+
+
+env = environ.Env()  # TODO: Change when about to deploy
+
+
+env_file = Path('.env')  # TODO: REMOVE OR COMMENT OUT WHEN ABOUT TO PUSH TO PRODUCTION
+
+environ.Env.read_env(env_file)
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+
+APP_DIR = ROOT_DIR / "core_apps"
+
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    # "drf_yasg",
+    "corsheaders",
+
+]
+
+
+LOCAL_APPS = [
+    "core_apps.account",
+    "core_apps.hawsr",
+]
+
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "hawsrproject.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "hawsrproject.wsgi.application"
+
+
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "api_key": {"type": "apiKey", "in": "header", "name": "Authorization"}
+    },
+    "USE_SESSION_AUTH": True,
+    "SHOW_REQUEST_HEADERS": True,
+    "DOC_EXPANSION": "list",
+    "JSON_EDITOR": True,
+    "SHOW_EXTENSIONS": True,
+    "VALIDATOR_URL": None,
+}
+
+# DATABASES = {"default": env.db("DATABASE_URL")}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "NON_FIELD_ERRORS_KEY": "errors",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/day',
+        'user': '10/minute',
+        'test-api': '5/day',
+        'Login-Throttle': '5/minute'
+    }
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=31),
+    "ROTATE_REFRESH_TOKENS": True,
+    "SIGNING_KEY": env("SIGNING_KEY"),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "ROTATE_REFRESH_TOKENS": True,
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Africa/Lagos"
+
+USE_I18N = True
+
+USE_TZ = True
+
+SITE_ID = 1
+
+ADMIN_URL = env("DJANGO_ADMIN_URL")
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
+
+# MEDIA_URL = 'http://localhost:8080/mediafiles/'  # TODO: change back to MEDIA_URL = "/mediafiles/" when going live
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# AUTH_USER_MODEL = "account.User"
+
+APPEND_SLASH = False
---
+++ hawsrproject/settings/local.py
@@ -0,0 +1,61 @@
+from .base import *  # noqa
+from .base import env
+
+
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
+CURRENT_ENV = env("CURRENT_ENV")
+
+
+ADMINS = [("Ilesanmi Temitope", "ilesanmiisaac@gmail.com")]
+
+# TODO:Change from localhost DOMAIN NAME  of the production server
+
+
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="oXPWQPA3C3sdBCuBeXUKq3LBp9YDJ33-306p9EAKf1ja1xkWnKY",
+)
+
+ALLOWED_HOSTS = ["localhost"]
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080",
+                        "http://localhost:5173", "http://localhost:3000",
+                        ]
+
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost",
+                        ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+ADMIN_URL = env("DJANGO_ADMIN_URL")
+
+
+SITE_NAME = "Hawsr"
+
+
+DOMAIN = env("DOMAIN")
+
+CORS_URLS_REGEX = r"^/api/.*$"
+
+
+if CURRENT_ENV == "local":
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+
+elif CURRENT_ENV == "live":
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+    SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+    # TODO: Change to 518400 later
+    SECURE_HSTS_SECONDS = 518400
+
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+
+    SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True)
